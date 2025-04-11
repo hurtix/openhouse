805b825afa6b3880ea76e2a39e5bc5f33e124fa5
hurtix <hurtix@gmail.com>
2025-04-11 16:37:18 -0500

    Add filter aside for mobile

--- theme/archive-proyecto.php
+++ theme/archive-proyecto.php
@@ -10,21 +10,21 @@
 get_header();
 ?>
 <div class="bg-[var(--dark)] h-auto lg:h-[25vh]">
-    <div class="text-white container mx-auto px-4 md:px-0 pt-30 md:pt-0 flex justify-end items-start h-full flex-col">
+    <div class="text-white container mx-auto px-4 lg:px-0 pt-30 lg:pt-0 flex justify-end items-start h-full flex-col">
         <h2 class="font-bold text-4xl">Proyectos que inspiran progreso</h2>
         <p>Explora nuestra selección de proyectos destacados y encuentra el lugar ideal para vivir, trabajar o invertir.</p>
     </div>
 </div>
 
-<div class="wrapper relative pt-10" id="archive-wrapper" style="background: linear-gradient(-180deg, rgb(33, 37, 41) 25vh, var(--color-gray-100) 25vh);">
-    <div class="container mx-auto px-4 md:px-0" id="content" tabindex="-1">
+<div class="wrapper pt-10" id="archive-wrapper" style="background: linear-gradient(-180deg, rgb(33, 37, 41) 25vh, var(--color-gray-100) 25vh);">
+    <div class="container mx-auto px-4 lg:px-0" id="content" tabindex="-1">
         <div class="flex flex-wrap">
             <div class="flex justify-between items-center mb-4 w-full">
-                <button id="btn-filtro" type="button" class="btn text-sm md:text-base inline-flex cursor-pointer text-white py-3 px-4 leading-none rounded-full border border-gray-200 hover:bg-white hover:text-[var(--dark)]" data-bs-toggle="button" data-filter-value="<?php echo esc_attr($term->slug); ?>"></button>
+                <button id="btn-filtro" type="button" class="btn text-sm lg:text-base inline-flex cursor-pointer text-white py-3 px-4 leading-none rounded-full border border-gray-200 hover:bg-white hover:text-[var(--dark)]" data-bs-toggle="button" data-filter-value="<?php echo esc_attr($term->slug); ?>"></button>
                 <span id="resultados" class="text-white"></span>
             </div>
             <div class="flex items-start gap-8">
-                <div id="wrapper-filtro" class="hidden mb-20 shadow-lg bg-white border border-gray-200 rounded-none md:rounded p-4">
+                <div id="wrapper-filtro" class="hidden mb-20 shadow-lg bg-white border border-gray-200 rounded-none lg:rounded p-4">
                     <a class="btn text-sm inline-flex  py-2 px-3 border border-[var(--red)] text-[var(--red)] hover:bg-[var(--red)] hover:text-white rounded-full" href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3 me-1" viewBox="0 0 16 16">
                             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
                         </svg> Borrar filtros</a>
@@ -110,7 +110,7 @@ get_header();
                         ?>
                     </ul>
                 </div>
-                <main id="main" class="site-main grid grid-cols-1 md:grid-cols-4 gap-8 md:gap-4 w-full pb-20">
+                <main id="main" class="site-main grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-4 w-full pb-20">
                     <?php
                     if (have_posts()) {
                         while (have_posts()) {
@@ -134,12 +134,13 @@ get_header();
         // Existing button toggle code
         const btnFiltro = document.getElementById('btn-filtro');
         const wrapperFiltro = document.getElementById('wrapper-filtro');
+        const resultados = document.getElementById('resultados');
         const main = document.getElementById('main');
         btnFiltro.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter me-1" viewBox="0 0 16 16"><path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/></svg> Filtrar';
         function updateButtonContent() {
             const isOpen = wrapperFiltro.classList.contains('isOpen');
             btnFiltro.innerHTML = isOpen ? 
-                `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg me-1" viewBox="0 0 16 16"><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/></svg> Cerrar filtro` : 
+                `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg lg:me-1" viewBox="0 0 16 16"><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/></svg> <?php echo wp_is_mobile() ? '' : 'Cerrar filtro'; ?>` : 
                 `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter me-1" viewBox="0 0 16 16"><path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/></svg> Filtrar`;
         }
 
@@ -156,6 +157,18 @@ get_header();
                 wrapperFiltro.classList.toggle('left-0');
                 wrapperFiltro.classList.toggle('w-full');
                 wrapperFiltro.classList.toggle('h-screen');
+                btnFiltro.classList.toggle('absolute');
+                btnFiltro.classList.toggle('z-100');
+                btnFiltro.classList.toggle('top-[11%]');
+                btnFiltro.classList.toggle('right-[4%]');
+                btnFiltro.classList.toggle('bg-[var(--dark)]');
+                btnFiltro.classList.toggle('aspect-square');
+                btnFiltro.classList.toggle('items-center');
+                resultados.classList.toggle('absolute');
+                resultados.classList.toggle('z-100');
+                resultados.classList.toggle('top-[16%]');
+                resultados.classList.toggle('right-[4%]');
+                resultados.classList.toggle('text-white');
                 document.body.classList.toggle('overflow-hidden');
         });
 <?php } else { ?>
@@ -166,8 +179,8 @@ get_header();
             wrapperFiltro.classList.toggle('w-[25%]');
             main.classList.toggle('w-[100%]');
             main.classList.toggle('w-75%]');
-            main.classList.toggle('md:grid-cols-4');
-            main.classList.toggle('md:grid-cols-3');
+            main.classList.toggle('lg:grid-cols-4');
+            main.classList.toggle('lg:grid-cols-3');
         });
 <?php } ?>
         
